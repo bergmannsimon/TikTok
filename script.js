852534f3ffdb0ac67f56c571e4d960b175985c90
NavyOtter <moritz.mikus@web.de>
2018-12-13 11:34:20 +0100

    Started coding isValid()

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-" " 
+
+function isValid(arr) {
+
+    let n, m, g, k;
+
+    for (let i in arr) {
+        for (let j in arr[i]) {
+            if (arr[i][j] == 'X'){
+                n++;
+            } else if (arr[i][j] == 'O') {
+                m++;
+            } else if (arr[i][j] == '') {
+                k++;
+            } else {
+                console.log("Error with two-dimensional playfield array");
+            }
+        }
+    }
+}
